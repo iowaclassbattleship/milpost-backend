6d9c6e471e51bca58e6af2b2a1b4b16fe779ce7a
kali <kali@kali.kali>
2020-03-24 14:05:46 +0100

    feat(): auth middleware

--- auth/auth.go
+++ auth/auth.go
@@ -1,8 +1,44 @@
 package auth
 
-import "fmt"
+import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+)
 
-func AuthenticateWithBasic(token string) bool {
-	fmt.Printf("hfhf")
-	return true
+func BasicAuth(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("WWW_Authenticate", `Basic realm="Restricted"`)
+
+		s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+		if len(s) != 2 {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		b, err := base64.StdEncoding.DecodeString(s[1])
+		if err != nil {
+			http.Error(w, err.Error(), 401)
+			return
+		}
+
+		pair := strings.SplitN(string(b), ":", 2)
+		if len(pair) != 2 {
+			http.Error(w, "Not authorized", 401)
+		}
+
+		if pair[0] != "username" || pair[1] != "password" {
+			http.Error(w, "Not authorized", 401)
+		}
+
+		h.ServeHTTP(w, r)
+	}
+}
+
+func Use(h http.HandlerFunc, middleware ...func(http.HandlerFunc) http.HandlerFunc) http.HandlerFunc {
+	for _, m := range middleware {
+		h = m(h)
+	}
+
+	return h
 }
