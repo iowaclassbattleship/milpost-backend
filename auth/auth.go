840ab6df12e6487e6c4f8fd67b748dee01e674bb
kali <kali@kali.kali>
2020-03-24 16:59:45 +0100

    feat(): improved errorhandling and token distribution works

--- auth/auth.go
+++ auth/auth.go
@@ -1,10 +1,18 @@
 package auth
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 
 	"milpost.ch/errorhandler"
 )
+
+type tokenResult struct {
+	Token string `json:"token"`
+}
 
 func BasicAuth(h http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +29,17 @@ func BasicAuth(h http.HandlerFunc) http.HandlerFunc {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user": "tester",
+		"exp":  time.Now().Add(time.Hour * time.Duration(1)).Unix(),
+		"iat":  time.Now().Unix(),
+	})
+	tokenString, err := token.SignedString([]byte("123"))
+	if errorhandler.IsError(err) == true {
+		errorhandler.JSONError(w, errorhandler.JSONErrorModel{"TOKEN_GENERATION_FAILED"}, http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte("hello"))
+	json.NewEncoder(w).Encode(tokenResult{tokenString})
 }
